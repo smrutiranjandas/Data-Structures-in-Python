7904d8e7454223e80779df45ab6b86ea64fc5730
smrutiranjandas <55105408+smrutiranjandas@users.noreply.github.com>
2022-08-29 11:19:14 +0530

    Add files via upload

---
+++ Recursion/Fibonnaci.py
@@ -0,0 +1,15 @@
+# Requirement: To find nth Fibonacci number
+# Method used: Recursion
+
+def findFibonacci(intNum):
+    
+    if intNum == 1 or intNum == 2:
+        return 1
+    else:
+        
+        intFib = findFibonacci(intNum-1) + findFibonacci(intNum-2)
+        return intFib
+        
+# Print fibonnaci numbers from 1 to 20        
+for i in range(1, 20):
+    print(findFibonacci(intNum=i))
